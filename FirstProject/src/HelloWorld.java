2b38f1b6c95e61401fa34b92d9160c567ad860fe
BIT504 <ummelaila@gmail.com>
2020-06-26 09:39:59 +1200

    I have done some small changes

--- FirstProject/src/HelloWorld.java
+++ FirstProject/src/HelloWorld.java
@@ -1,9 +1,9 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello World");
+	public static void main(String[] args) 
+	{
+		 System.out.println("Hello World");
 	}
 
 }
